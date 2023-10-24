3a868cd3244bed086f17bb50336b788e8bd01add
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2023-10-24 12:50:34 +0200

    bump ruby version

--- manifests/install.pp
+++ manifests/install.pp
@@ -37,7 +37,7 @@ class r10k::install (
                     } elsif (versioncmp("${::kernelversion}", '6.7') < 0) { #lint:ignore:only_variable_string
                       $real_package_name = 'ruby26-r10k'
                     } else {
-                      $real_package_name = 'ruby31-r10k'
+                      $real_package_name = 'ruby32-r10k'
                     }
                   }
       'portage': { $real_package_name = 'app-admin/r10k' }
